5141e05364720602000076e1653dacd15b612e63
Vishal Sharma <vsh1819@gmail.com>
2018-07-19 08:57:45 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,4 +19,20 @@ class RecipesController < ApplicationController
     redirect_to recipe_path
   end
   
+  
+  private
+  
+  def person_params
+  params.require(:person).permit(
+    :name,
+    addresses_attributes: [
+      :street_address_1,
+      :street_address_2,
+      :city,
+      :state,
+      :zipcode,
+      :address_type
+    ]
+  )
+end
 end
